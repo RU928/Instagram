30b8eb48cb4f8b0753ac5809997aed63db553ff0
田村崚 <ryo@tamuranoMacBook-Pro.local>
2017-05-02 20:43:03 +0900

    ログイン画面、設定画面の作成

---
+++ Instagram/SettingViewController.swift
@@ -0,0 +1,89 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 田村崚 on 2017/05/02.
+//  Copyright © 2017年 ryo.tamura. All rights reserved.
+//
+
+import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
+import SVProgressHUD
+
+class SettingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    @IBAction func handleChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text{
+            
+            if displayName.characters.isEmpty {
+                SVProgressHUD.showError(withStatus: "表示名を入力してください")
+                return
+            }
+            
+            let user =  FIRAuth.auth()?.currentUser
+            if let user = user{
+            let changeRequest = user.profileChangeRequest()
+            changeRequest.displayName = displayName
+                changeRequest.commitChanges{ error in
+                    if let error = error{
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName)]の設定に成功しました。")
+                    SVProgressHUD.showSuccess(withStatus: "表示名を変更しました")
+                }
+            }else {
+                print("DEBUG_PRINT: displayNameの設定に失敗しました。")
+            }
+            //ここまでif文
+        }
+        //上までfunction内最上位のif文
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        
+        try! FIRAuth.auth()?.signOut()
+        
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
+        
+        // ログイン画面から戻ってきた時のためにホーム画面（index = 0）を選択している状態にしておく
+        let tabBarController = parent as! ESTabBarController
+        tabBarController.setSelectedIndex(0, animated: false)
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let user = FIRAuth.auth()?.currentUser
+        if let user = user{
+            displayNameTextField.text = user.displayName
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
